5791afd4da8279eba5ad59da925d066768012749
Abdurrahim <abdurrahim22code@gmail.com>
2025-01-30 16:43:44 +0800

    Add js code

---
+++ Logins/Login-06/script.js
@@ -0,0 +1,12 @@
+const formOpenBtn = document.querySelector("form-open"),
+    home = document.querySelector(".home"),
+    formContainer = document.querySelector(".form-container"),
+    formCloseBtn = document.querySelector(".form-close"),
+    signupBtn = document.querySelector("#signup"),
+    loginBtn = document.querySelector("#login"),
+    pwShowHide = document.querySelectorAll(".pw-hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+
