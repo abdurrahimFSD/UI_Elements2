6b18e49066d18bb834577af0e7c97dc2e686bc3c
Abdurrahim <abdurrahim22code@gmail.com>
2025-01-31 10:38:53 +0800

    Update js

--- Logins/Login-06/script.js
+++ Logins/Login-06/script.js
@@ -9,4 +9,15 @@ const formOpenBtn = document.querySelector("form-open"),
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 
-
+pwShowHide.forEach((icon) => {
+    icon.addEventListener("click", () => {
+        let getPwInput = icon.parentElement.querySelector("input");
+        if (getPwInput.type === "password") {
+            getPwInput.type = "text";
+            icon.classList.replace("uil-eye-slash", "uil-eye");
+        } else {
+            getPwInput.type = "password";
+            icon.classList.replace("uil-eye", "uil-eye-slash");
+        }
+    });
+});
