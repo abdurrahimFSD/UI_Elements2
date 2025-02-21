0a94416dcc32c12635ece43534c8d345cad16bff
Abdurrahim <abdurrahim22code@gmail.com>
2025-02-21 10:04:42 +0800

    Update js code

--- Logins/Login-09/script.js
+++ Logins/Login-09/script.js
@@ -6,5 +6,23 @@ const forms = document.querySelector(".forms"),
 pwShowHide.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {
         let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-    })
-})
+
+        pwFields.forEach(password => {
+            if (password.type === "password") { // If password is hidden
+                password.type = "text"; // Show password
+                eyeIcon.classList.replace("bx-hide", "bx-show");    // Replace eye icon to show state 
+                return;
+            }
+            password.type = "password"; // Hide password
+            eyeIcon.classList.replace("bx-show", "bx-hide");    // Replace eye icon to hide state
+        });
+    });
+});
+
+// Add click event listener to switch between login and signup forms
+links.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        forms.classList.toggle("show-signup");
+    });
+});
