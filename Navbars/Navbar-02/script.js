a656928cd3b60d6e7ad58b0383450e7ffba151c0
Abdurrahim <abdurrahim22code@gmail.com>
2026-01-12 10:03:57 +0800

    feat(script): enable mobile navigation open and close actions

--- Navbars/Navbar-02/script.js
+++ Navbars/Navbar-02/script.js
@@ -11,3 +11,12 @@ searchIcon.addEventListener("click", () => {
     }
     searchIcon.classList.replace("uil-times", "uil-search");
 });
+
+navOpenBtn.addEventListener("click", () => {
+    nav.classList.add("openNav");
+    nav.classList.remove("openSearch");
+    searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+    nav.classList.remove("openNav");
+});
