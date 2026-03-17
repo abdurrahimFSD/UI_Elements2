7873c081e116fe8534b42e8105eb8893a93d740d
Abdurrahim <abdurrahim22code@gmail.com>
2026-03-17 11:12:57 +0800

    feat(js): implement search box toggle and dynamic icon swap

---
+++ Navbars/Navbar-09/script.js
@@ -0,0 +1,12 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+
+searchBox.addEventListener("click", ()=>{
+    navbar.classList.toggle("showInput");
+    if(navbar.classList.contains("showInput")){
+        searchBox.classList.replace("bx-search" ,"bx-x");
+    }else {
+        searchBox.classList.replace("bx-x" ,"bx-search");
+    }
+});
