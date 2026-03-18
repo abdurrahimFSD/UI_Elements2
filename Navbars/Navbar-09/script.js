5f0e6851121b7e1bd25c0f4223b1991776bf6609
Abdurrahim <abdurrahim22code@gmail.com>
2026-03-18 12:02:24 +0800

    feat(js): add open and close functionality for mobile sidebar

--- Navbars/Navbar-09/script.js
+++ Navbars/Navbar-09/script.js
@@ -10,3 +10,14 @@ searchBox.addEventListener("click", ()=>{
         searchBox.classList.replace("bx-x" ,"bx-search");
     }
 });
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+    navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+    navLinks.style.left = "-100%";
+}
