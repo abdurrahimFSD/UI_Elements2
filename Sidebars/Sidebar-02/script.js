b3ce72151e457461e6c47ff195ada813c15e02f8
Abdurrahim <abdurrahim22code@gmail.com>
2026-09-14 11:02:37 +0800

    feat(js): add function to close all open dropdown menus

--- Sidebars/Sidebar-02/script.js
+++ Sidebars/Sidebar-02/script.js
@@ -3,3 +3,10 @@ const toggleDropdown = (dropdown, menu, isOpen) => {
     dropdown.classList.toggle("open", isOpen);
     menu.style.height = isOpen ? `${menu.scrollHeight}px` : 0;
 };
+
+// Close all open dropdowns
+const closeAllDropdowns = () => {
+    document.querySelectorAll(".dropdown-container.open").forEach((openDropdown) => {
+        toggleDropdown(openDropdown, openDropdown.querySelector(".dropdown-menu"), false);
+    });
+}
